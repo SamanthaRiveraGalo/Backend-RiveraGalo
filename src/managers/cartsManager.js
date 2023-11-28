bc57af7b0e3d98d8acc9855aeda6fe8c5256ad5f
Samantha Rivera Galo <samirivera1808@gmail.com>
2023-11-28 19:13:14 -0300

    cartmanager cambio en las funciones

--- src/managers/cartsManager.js
+++ src/managers/cartsManager.js
@@ -1,70 +1,70 @@
 const fs = require('fs')
 
-//tengo que traer el json de productos 
-
 class CartsManager {
     constructor(path) {
         this.path = path
-    }
-
-    readFile = async () => {
-        try {
-            const dataCart = await fs.promises.readFile(this.path, 'utf-8')
-            return JSON.parse(dataCart)
-        } catch {
-            return []
-        }
+        this.cart = []
     }
 
     //crear carrito
     createCart = async () => {
-        const carts = this.readFile()
+
         let newCart
-        if (carts.lenght === 0) {
+        if (this.cart.lenght === 0) {
             newCart = { id: 1, products: [] }
         } else {
-            newCart = { id: carts.lenght + 1, products: [] }
+            newCart = { id: this.cart.lenght + 1, products: [] }
         }
-        carts.push(newCart)
-        const results = await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2), 'utf-8')
-        return results
+
+        this.cart.push(newCart)
+
+        const jsonCart = JSON.stringify(this.cart);
+        await fs.promises.writeFile(this.path, jsonCart);
     }
 
     //por id
     getCartById = async (cid) => {
-        const carts = await this.readFile()
-        const cart = carts.find(cart => cart.id === cid)
+
+        const cartString = await fs.promises.readFile(this.path, 'utf-8');
+        const cartArray = JSON.parse(cartString);
+
+        const cart = cartArray.find(cart => cart.id === cid);
+
         if (!cart) {
-            return 'No se encontro el carrito'
+            return 'No se encontro el carrito';
         }
-        return cart
+
+        return cart;
+
     }
 
     //agregar un producto
     addProductToCart = async (cid, pid) => {
         //leo el archivo
-        const carts = await this.readFile()
+        const cartString = await fs.promises.readFile(this.path, 'utf-8');
+        const cartArray = JSON.parse(cartString);
+
         //busco el cart por el id
-        const cartIndex = carts.findIndex(cart => cart.id === cid)
+        const cartIndex = cartArray.findIndex(cart => cart.id === cid)
 
         if (cartIndex !== -1) {
             return 'No se encuentra el carrito'
         }
-        
+
         //busco el producto en el carrito con el id
-        const productCarts = carts[cartIndex].products.find(product => product.id === pid)
+        const productCarts = cartArray[cartIndex].products.find(product => product.id === pid)
 
         //si el producto no existe, lo agrego
         if (!productCarts) {
-            carts[cartIndex].products.push({ id: pid, quantity: 1 })
+            cartArray[cartIndex].products.push({ id: pid, quantity: 1 })
         } else {
             //si el producto existe, incremento la cantidad
             productCarts.quantity++
         }
         //luego lo escribo
-        const results = await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2), 'utf-8')
+        const results = await fs.promises.writeFile(this.path, JSON.stringify(cartArray, null, 2), 'utf-8')
         return results
+
     }
 }
-
 module.exports = CartsManager
