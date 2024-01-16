7d1a5141870e972eae9163afdbcd4468ba3c184f
Samantha Rivera Galo <samirivera1808@gmail.com>
2024-01-16 11:39:37 -0300

    register.js

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,16 +1,52 @@
+// const form = document.getElementById('registerForm');
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     const data = new FormData(form);
+//     const obj = {};
+//     data.forEach((value, key) => obj[key] = value);
+//     fetch('/api/sessions/register', {
+//         method: 'POST',
+//         body: JSON.stringify(obj),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     }).then(result => {
+//         if (result.status === 200) {
+//             window.location.replace('/views/login');
+//         }
+//     });
+// });
 const form = document.getElementById('registerForm');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    const data = new FormData(form);
-    const obj = {};
-    data.forEach((value, key) => obj[key] = value);
+
+    const formData = new FormData(form);
+
+    const firstName = document.getElementById('firstName')
+    const lastName = document.getElementById('lastName')
+    const email = document.getElementById('email')
+    const password = document.getElementById('password')
+
+    const firstNameValue = firstName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+
+    const newUser = {
+        first_name: firstNameValue,
+        last_name: lastNameValue,
+        email: emailValue,
+        password: passwordValue
+    }
+
     fetch('/api/sessions/register', {
         method: 'POST',
-        body: JSON.stringify(obj),
         headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify(newUser)
     }).then(result => {
         if (result.status === 200) {
             window.location.replace('/views/login');
